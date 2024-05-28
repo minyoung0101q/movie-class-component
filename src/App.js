c473b80d1c463657315c66bc974795079cf4b202
stylish_code <minyoung0101q@gmail.com>
2024-05-28 21:45:35 +0900

    #10.1 Reusable Components with JSX + Props

--- src/App.js
+++ src/App.js
@@ -1,25 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+
+function Food(props) { // 구조 분해 할당 : 객체나 배열의 속성을 쉽게 추출
+  console.log(props);
+  return (
+    <div>
+      <h3>I like {props.favorite}</h3>
+      <h3>{props.good}</h3>
+    </div>
+  )
+}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>I love you.</h1>
+      <Food favorite="kimchi" good="you" />
+      <Food favorite="ramen" />
+      <Food favorite="smagiopsal" />
+      <Food favorite="chukumi" />
     </div>
-  );
+  )
 }
 
 export default App;
