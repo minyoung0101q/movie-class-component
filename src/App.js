dbc36bae43d95f30c46e50b03eb2d54dad1c5b9d
stylish_code <minyoung0101q@gmail.com>
2024-06-13 14:43:13 +0900

    #11.1 All you need to know about State

--- src/App.js
+++ src/App.js
@@ -4,17 +4,17 @@ class App extends React.Component {
     state = {
         count: 0
     }
-    add() { // add = () =>
-        console.log("add");
-    };
-    minus() { // minus = () =>
-        console.log("minus");
+    add = () => {
+        this.setState(current => ({ count: current.count + 1 }));
+    }
+    minus = () => {
+        this.setState(current => ({ count: current.count - 1 }));
     }
 
     render() {
         return (
             <div>
-                <h1>The number is : {this.state.count}</h1>
+                <h2>This is number : {this.state.count}</h2>
                 <button onClick={this.add}>Add</button>
                 <button onClick={this.minus}>Minus</button>
             </div>
