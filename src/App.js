c91aed29c3ad0ac10d9e092d367d3baf17341998
stylish_code <minyoung0101q@gmail.com>
2024-06-14 01:36:11 +0900

    #12.1 Rendering the Movies

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
-/* movie data 받아오기 */
 import React from "react";
 import axios from "axios";
+import Movie from "./Movie";
 
 class App extends React.Component {
     state = {
@@ -9,21 +9,33 @@ class App extends React.Component {
     }
 
     getMovies = async () => {
-        const movies = await axios.get("https://yts.mx/api/v2/list_movies.json");
-        console.log(movies);
+        const { data: { data: { movies } } } = await axios.get("https://yts.mx/api/v2/list_movies.json?sort_by=rating");
+        console.log(movies); // 받아온 데이터를 movies state에 넣어주고, loading 상태를 false로 변경
+        // 받아온 데이터는 객체로 이루어진 배열
+
+        this.setState({ movies, isLoading: false }); // movies state에 데이터 넣기 / loading 상태 변경
     }
 
-    componentDidMount() {
+    componentDidMount() { // 생명주기 메서드
         this.getMovies();
     }
 
     render() {
-
-        const { isLoading } = this.state;
-
+        const { isLoading, movies } = this.state;
         return (
             <div>
-                <h2>{isLoading ? "Loading..." : "We are ready."}</h2>
+                {isLoading ? "Loading..."
+                    : movies.map(movie => (
+                        <Movie
+                            key={movie.id}
+                            id={movie.id}
+                            year={movie.year}
+                            title={movie.title}
+                            summary={movie.summary}
+                            poster={movie.medium_cover_image}
+                        />
+                    ))
+                }
             </div>
         )
     }
