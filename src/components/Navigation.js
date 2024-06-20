53b0112f337bd0b55a690bbe224010ce8470b40c
stylish_code <minyoung0101q@gmail.com>
2024-06-21 01:33:59 +0900

    #14.3 Sharing Props Between Routes

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom";
 
 function Navigation() {
     return (
-        <div>
+        <div className="nav">
             <Link to="/">Home</Link>
             <Link to="/about">About</Link>
         </div>
--- src/routes/About.js
+++ src/routes/About.js
@@ -1,8 +1,14 @@
 import React from "react";
+import "./About.css";
 
-function About() {
+function About(props) {
+    console.log(props);
     return (
-        <div>About Page</div>
+        <div className="about__container">
+            <span className="about__text">
+                "Freedom is the freedom, to say that two plus two make four."
+            </span>
+        </div>
     )
 }
 
